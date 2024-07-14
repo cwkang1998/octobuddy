47aac33eceb7026e6267127be805b2f47c1f5968
yenxuan1381 <emilylo1381@gmail.com>
2024-07-14 06:53:20 +0100

    feat: added login page

--- main-app/app/page.tsx
+++ main-app/app/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-
-import ContactsList from "./components/contacts";
-import CreateGroup from "./components/createGroup";
-import GroupTransactions from "./components/groupTxn";
-import Home from "./components/home";
-import InvitedLink from "./components/invitedLink";
-import ManageGroup from "./components/manageGroup";
-import RecentTransactions from "./components/recentTransactions";
-import Dashboard from "./dashboard";
+import Login from "./components/login";
 
 export default function Page() {
-	return <Home />;
+    return <Login />;
 }
